b1d75eaf2c61c865545681a90899313c01639aea
shubham-chauhan1992 <shubham-chauhan1992>
2024-08-20 03:21:48 +0000

    delta live tables

---
+++ notebook_11:Delta Live Tables via Python.py
@@ -0,0 +1,53 @@
+# Databricks notebook source
+import dlt
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+# COMMAND ----------
+
+
+source_file="/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-uncompressed-json/2015_2_clickstream.json"
+@dlt.table(
+    comment="The raw wikipedia clickstream dataset, ingested from /databricks-datasets."
+)
+def clickstream_raw():          
+  return (
+    spark.read.format("json").load(source_file)
+  )
+
+
+
+# COMMAND ----------
+
+@dlt.table(
+  comment="Wikipedia clickstream data cleaned and prepared for analysis."
+)
+@dlt.expect("valid_current_page_title", "current_page_title IS NOT NULL")
+@dlt.expect_or_fail("valid_count", "click_count > 0")
+def clickstream_prepared():
+  return (
+    dlt.read("clickstream_raw")
+      .withColumn("click_count", expr("CAST(n AS INT)"))
+      .withColumnRenamed("curr_title", "current_page_title")
+      .withColumnRenamed("prev_title", "previous_page_title")
+      .select("current_page_title", "click_count", "previous_page_title")
+  )
+
+# COMMAND ----------
+
+@dlt.table(
+  comment="A table containing the top pages linking to the Apache Spark page."
+)
+def top_spark_referrers():
+  return (
+    dlt.read("clickstream_prepared")
+      .filter(expr("current_page_title == 'Apache_Spark'"))
+      .withColumnRenamed("previous_page_title", "referrer")
+      .sort(desc("click_count"))
+      .select("referrer", "click_count")
+      .limit(10)
+  )
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs("/delta_live_tables")
